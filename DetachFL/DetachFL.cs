4ccab763423fbcf873638f5da4c44d11cc5f3b45
Vildar Khisyametdinov <vildar82@gmail.com>
2016-09-01 09:37:26 +0300

    Добавил вчерашние наработки

--- DetachFL/DetachFL.cs
+++ DetachFL/DetachFL.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.AECC.Interop.Land;
 
 namespace DetachFL
 {
@@ -28,27 +29,58 @@ namespace DetachFL
         public void Test()
         {
             var civil = CivilApplication.ActiveDocument;
-            var surfIds = civil.GetSurfaceIds();  
+            var surfIds = civil.GetSurfaceIds();
 
             var selOPt = new PromptEntityOptions("\nВыбери хар линию");
             selOPt.SetRejectMessage("\nТолько хар линию");
             selOPt.AddAllowedClass(typeof(FeatureLine), true);
             var selRes = ed.GetEntity(selOPt);
-            if (selRes.Status != PromptStatus.OK                 )
+            if (selRes.Status != PromptStatus.OK)
             {
                 return;
             }
-            var fl = selRes.ObjectId.GetObject(OpenMode.ForRead) as FeatureLine;           
+            var fl = selRes.ObjectId.GetObject(OpenMode.ForRead) as FeatureLine;
 
-            var settingsFl = civil.Settings.GetSettings<SettingsCmdAddSurfaceBreaklines>();
-            
             foreach (ObjectId surfId in surfIds)
             {
                 var surf = surfId.GetObject(OpenMode.ForRead) as TinSurface;
-                var brLines = surf.BreaklinesDefinition;
-                var count = brLines.Count;
-                var br = brLines[0];
+                IAeccTinSurface surfCom = (IAeccTinSurface)surf.AcadObject;
+                var rettyuy = surf.AcadObject;
+                var tttt = rettyuy.GetType();
+
+                for (int i = 0; i < surfCom.Breaklines.Count; i++)
+                {
+                    var brLine = surfCom.Breaklines.Item(i);
+                    var brLineEnts = (object[])brLine.BreaklineEntities;
+                    List<ObjectId> idEntsToAdd = new List<ObjectId>();
+                    bool isFind = false;
+                    for (int b = 0; b < brLineEnts.Length; b++)
+                    {
+                        var brLineId = Autodesk.AutoCAD.DatabaseServices.DBObject.FromAcadObject(brLineEnts[b]);
+                        idEntsToAdd.Add(brLineId);
+                        if (fl.Id == brLineId)
+                        {
+                            surf.BreaklinesDefinition.RemoveAt(i);
+                            idEntsToAdd.Remove(brLineId);
+                            isFind = true;
+                        }
+                    }
+                    if (isFind)
+                    {
+                        if (idEntsToAdd.Any())
+                        {
+                            AddOperToSurf(surf, idEntsToAdd);
+                        }
+                        return;
+                    }
+                }
             }
+        }
+
+        private void AddOperToSurf (TinSurface surf, List<ObjectId> idEntsToAdd)
+        {
+            ObjectIdCollection ids = new ObjectIdCollection(idEntsToAdd.ToArray());
+            surf.BreaklinesDefinition.AddStandardBreaklines(ids, 0.1, 0, 0, 0);
         }
     }
 }
